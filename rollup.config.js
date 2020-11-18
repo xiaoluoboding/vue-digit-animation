f92c4773761e4c9346006b7efee236fd48cfaed1
xiaoluoboding <xiaoluoboding@gmail.com>
2020-11-18 19:00:54 +0800

    chore: rebuild external with vue-demi

--- rollup.config.js
+++ rollup.config.js
@@ -134,7 +134,7 @@ function createConfig (format, output, plugins = []) {
   // during a single build.
   hasTSChecked = true
 
-  const external = ['vue']
+  const external = ['vue-demi']
 
   const nodePlugins = [resolve(), commonjs()]
 
